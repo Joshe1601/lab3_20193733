e080aef0272284bb76953540ec11604b637f0be5
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-08-31 22:01:20 -0500

    Se añadió codigo a el controlador de jugadores

--- src/main/java/com/example/lab3_20193733/controlador/JugadorController.java
+++ src/main/java/com/example/lab3_20193733/controlador/JugadorController.java
@@ -3,6 +3,7 @@ package com.example.lab3_20193733.controlador;
 import com.example.lab3_20193733.entidad.Jugador;
 import com.example.lab3_20193733.repositorio.JugadorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,11 +17,12 @@ public class JugadorController {
     }
 
     @GetMapping(value = "/jugador")
-    public String listarJugadores() {
+    public String listarJugadores(Model model) {
         List<Jugador> jugadores = jugadorRepository.findAll();
+        model.addAttribute("listaJugadores", jugadores);
         for (Jugador jugador : jugadores) {
             System.out.println(jugador.getNombre());
         }
-        return "jugador";
+        return "jugadores/lista-jugadores";
     }
 }
--- src/main/java/com/example/lab3_20193733/entidad/Jugador.java
+++ src/main/java/com/example/lab3_20193733/entidad/Jugador.java
@@ -1,7 +1,9 @@
 package com.example.lab3_20193733.entidad;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "Jugador")
 public class Jugador {
@@ -26,48 +28,24 @@ public class Jugador {
     @Column(name = "sn_idSeleccion", nullable = false)
     private int sn_idSeleccion;
 
-    public int getIdJugador() {
-        return idJugador;
-    }
-
     public void setIdJugador(int idJugador) {
         this.idJugador = idJugador;
-    }
-
-    public String getNombre() {
-        return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public int getEdad() {
-        return edad;
-    }
-
     public void setEdad(int edad) {
         this.edad = edad;
-    }
-
-    public String getPosicion() {
-        return posicion;
     }
 
     public void setPosicion(String posicion) {
         this.posicion = posicion;
     }
 
-    public String getClub() {
-        return club;
-    }
-
     public void setClub(String club) {
         this.club = club;
-    }
-
-    public int getSn_idSeleccion() {
-        return sn_idSeleccion;
     }
 
     public void setSn_idSeleccion(int sn_idSeleccion) {
